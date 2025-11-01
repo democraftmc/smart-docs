ade48257e78b0f550a18ba18547078ad52fc020f
Funasitien DreamCloud <hello@funa.dev>
2025-11-01 16:09:49 +0100

    🐛 Fix Metadatas

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,25 @@ import { RootProvider } from 'fumadocs-ui/provider/next';
 import './global.css';
 import { Plus_Jakarta_Sans } from 'next/font/google';
 import SearchDialog from '@/components/search';
+import type { Metadata } from 'next/types';
 
 const plusJakartaSans = Plus_Jakarta_Sans({
   subsets: ['latin'],
+});
+
+export function createMetadata(override: Metadata): Metadata {
+  return {
+    ...override,
+  };
+}
+
+export const metadata = createMetadata({
+  title: {
+    template: '%s | Smart',
+    default: 'Smart',
+  },
+  description: 'Dynamically create & delete server, with ANY provider, and the magic of RustyConnector',
+  metadataBase: "https://smart.democraft.fr",
 });
 
 export default function Layout({ children }: LayoutProps<'/'>) {
