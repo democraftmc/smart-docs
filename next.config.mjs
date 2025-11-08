eed712b26c171f8b4ecf65694c72d0af9587eef0
Funasitien 🌥 <hello@funa.dev>
2025-11-08 15:18:38 +0100

    🐛 Redirect again again

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,15 @@ const config = {
   output: 'export',
   trailingSlash: true,
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/discord',
+        destination: 'https://discord.gg/utJYSHx9JF',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default withMDX(config);
