37905293f46f6a21c275fa6e19b822e5ed697430
Funasitien DreamCloud <hello@funa.dev>
2025-11-08 17:45:50 +0100

    🐛 Fix discord redirect & add ci cache

--- next.config.mjs
+++ next.config.mjs
@@ -7,15 +7,6 @@ const config = {
   output: 'export',
   trailingSlash: true,
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: '/discord',
-        destination: 'https://discord.gg/utJYSHx9JF',
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default withMDX(config);
